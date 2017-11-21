43f76585b29e557ca3b4a1122d8a4aa4d21aa9ea
David Murphy <david@murphytech.ie>
2017-11-21 22:46:28 +0000

    changed arg

--- IoTCamera.py
+++ IoTCamera.py
@@ -119,7 +119,7 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 					path = "/{base_path}/{timestamp}.jpg".format(
 					    base_path=conf["dropbox_base_path"], timestamp=ts)
 					client.files_upload(open(t.path, "rb").read(), path)
-					Email(t.path)
+					Email(path)
 					t.cleanup()
 
 				# update the last uploaded timestamp and reset the motion
