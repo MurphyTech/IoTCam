ae0c4e70704be77b2081527f87ca035d19bc7859
David Murphy <david@murphytech.ie>
2017-11-22 16:55:09 +0000

    using single quotes, and 'r' param

--- client/clientemail.py
+++ client/clientemail.py
@@ -10,7 +10,9 @@ from email import encoders
 
 class Email():
     def __init__(self, filename):
-        config = json.load(open("conf.json"))
+        config = json.load(open('conf.json', 'r'))
+
+        print config
 
         msg = MIMEMultipart()
         msg['From'] = config["fromaddr"]
